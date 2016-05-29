2c287e27d75ba1de142e3952f8b12356276f06e8
Stan Smith <choicedesign@gmail.com>
2016-05-28 19:35:48 -0600

    Setup HTML and Hello world

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Stan Smith on 5/28/16.
+ */
+
+(function ($) {
+  'use strict';
+  
+  // UI Hello world
+  $('#helloWorld').html('Oh, it&rsquo;s on now!');
+
+  init();
+
+  function init() {
+    console.log('Hello world!');
+  }
+
+})(Zepto);
