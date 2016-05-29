eeba43c93d108e94a28eff2f0a175fcd8447a00a
Stan Smith <choicedesign@gmail.com>
2016-05-28 20:58:09 -0600

    Update randomize method
    Use Fisher–Yates Shuffle algorithm and add more items to each of the
    mock data arrays

--- src/js/app.js
+++ src/js/app.js
@@ -8,53 +8,83 @@
   var mockData = {
     'images': [
       {
-        'imageName': 'ImageName',
-        'imageUrl': 'ImageUrl',
-        'imageAlt': 'ImageAlt',
-        'imageAuthor': 'ImageAuthor'
+        'imageName': 'ImageName1',
+        'imageUrl': 'ImageUrl1',
+        'imageAlt': 'ImageAlt1',
+        'imageAuthor': 'ImageAuthor1'
       },
       {
-        'imageName': 'ImageName',
-        'imageUrl': 'ImageUrl',
-        'imageAlt': 'ImageAlt',
-        'imageAuthor': 'ImageAuthor'
+        'imageName': 'ImageName2',
+        'imageUrl': 'ImageUrl2',
+        'imageAlt': 'ImageAlt2',
+        'imageAuthor': 'ImageAuthor2'
       },
       {
-        'imageName': 'ImageName',
-        'imageUrl': 'ImageUrl',
-        'imageAlt': 'ImageAlt',
-        'imageAuthor': 'ImageAuthor'
+        'imageName': 'ImageName3',
+        'imageUrl': 'ImageUrl3',
+        'imageAlt': 'ImageAlt3',
+        'imageAuthor': 'ImageAuthor3'
+      },
+      {
+        'imageName': 'ImageName4',
+        'imageUrl': 'ImageUrl4',
+        'imageAlt': 'ImageAlt4',
+        'imageAuthor': 'ImageAuthor4'
+      },
+      {
+        'imageName': 'ImageName5',
+        'imageUrl': 'ImageUrl5',
+        'imageAlt': 'ImageAlt5',
+        'imageAuthor': 'ImageAuthor5'
       }
     ],
     'sounds': [
       {
-        'soundName': 'SoundName',
-        'soundUrl': 'SoundUrl',
-        'soundAuthor': 'SoundAuthor'
+        'soundName': 'SoundName1',
+        'soundUrl': 'SoundUrl1',
+        'soundAuthor': 'SoundAuthor1'
       },
       {
-        'soundName': 'SoundName',
-        'soundUrl': 'SoundUrl',
-        'soundAuthor': 'SoundAuthor'
+        'soundName': 'SoundName2',
+        'soundUrl': 'SoundUrl2',
+        'soundAuthor': 'SoundAuthor2'
       },
       {
-        'soundName': 'SoundName',
-        'soundUrl': 'SoundUrl',
-        'soundAuthor': 'SoundAuthor'
+        'soundName': 'SoundName3',
+        'soundUrl': 'SoundUrl3',
+        'soundAuthor': 'SoundAuthor3'
+      },
+      {
+        'soundName': 'SoundName4',
+        'soundUrl': 'SoundUrl4',
+        'soundAuthor': 'SoundAuthor4'
+      },
+      {
+        'soundName': 'SoundName5',
+        'soundUrl': 'SoundUrl5',
+        'soundAuthor': 'SoundAuthor5'
       }
     ],
     'texts': [
       {
-        'textContent': 'TextContent',
-        'textAuthor': 'TextAuthor'
+        'textContent': 'TextContent1',
+        'textAuthor': 'TextAuthor1'
       },
       {
-        'textContent': 'TextContent',
-        'textAuthor': 'TextAuthor'
+        'textContent': 'TextContent2',
+        'textAuthor': 'TextAuthor2'
       },
       {
-        'textContent': 'TextContent',
-        'textAuthor': 'TextAuthor'
+        'textContent': 'TextContent3',
+        'textAuthor': 'TextAuthor3'
+      },
+      {
+        'textContent': 'TextContent4',
+        'textAuthor': 'TextAuthor4'
+      },
+      {
+        'textContent': 'TextContent5',
+        'textAuthor': 'TextAuthor5'
       }
     ]
   };
@@ -70,24 +100,39 @@
     var images, sounds, texts = [];
 
     // TODO: Create a randomized array of each item
-    images = randomizeData(mockData.images);
-    sounds = randomizeData(mockData.sounds);
-    texts = randomizeData(mockData.texts);
+    images = shuffle(mockData.images);
+    sounds = shuffle(mockData.sounds);
+    texts = shuffle(mockData.texts);
 
     console.log(images);
     console.log(sounds);
     console.log(texts);
   }
 
-  function randomizeData(data) {
-    var randomizedData = [];
 
-    $.each(data, function (index, item) {
-      console.log(item);
-      randomizedData[index] = item;
-    });
+  /**
+   * Fisher–Yates Shuffle
+   * Via: https://bost.ocks.org/mike/shuffle/
+   *
+   * @param array
+   * @returns {*}
+   */
+  function shuffle(array) {
+    var m = array.length, t, i;
 
-    return randomizedData;
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[ m ];
+      array[ m ] = array[ i ];
+      array[ i ] = t;
+    }
+
+    return array;
   }
 
 })(Zepto);
